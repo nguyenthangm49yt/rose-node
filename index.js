b7ffec5b8ced8ca269e53f08615553b9fa3521fb
thangnm <thangnm@thangnms-MacBook-Air.local>
2023-03-06 22:05:02 +0700

    Add code

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // Add Express
 const express = require("express");
-
+const path = require('path');
+var cors = require('cors')
 // Initialize Express
 const app = express();
+app.use(cors())
 
+app.use('/', express.static(path.join(__dirname, "public")));
 // Create GET request
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  // loadMusic(res);
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+const loadMusic = (res) => {
+  res.sendFile(path.join(__dirname, '/public/music/I-Do-Duc-Phuc-911.mp3'));
+}
 // Initialize server
 app.listen(3000, () => {
   console.log("Running on port 3000.");
